07a2a39db44a5c09fbabfc1e38198388788a1d01
tdsdemimei <1794055180@qq.com>
2022-05-10 16:20:20 +0800

    第三次提交

--- src/main/java/com/tjw/community/util/MailClient.java
+++ src/main/java/com/tjw/community/util/MailClient.java
@@ -1,6 +1,5 @@
 package com.tjw.community.util;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ import javax.mail.internet.MimeMessage;
 
 @Component
 public class MailClient {
+
     private static final Logger logger = LoggerFactory.getLogger(MailClient.class);
 
     @Autowired
@@ -22,18 +22,18 @@ public class MailClient {
     @Value("${spring.mail.username}")
     private String from;
 
-    public void sendMail(String to, String subject, String content){
+    public void sendMail(String to, String subject, String content) {
         try {
-            //mailSender.createMimeMessage()创建的MimeMessage是空的，只是一个模板。使用MimeMessageHelper补充MimeMessage的内容
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
             helper.setFrom(from);
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(content,true);
+            helper.setText(content, true);
             mailSender.send(helper.getMimeMessage());
         } catch (MessagingException e) {
-            e.printStackTrace();
+            logger.error("发送邮件失败:" + e.getMessage());
         }
     }
+
 }
